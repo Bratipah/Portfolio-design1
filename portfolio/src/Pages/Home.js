56a22373fbd22b2653ec7e8578a09b47e6946f0c
Bratipah <bratipahmh@gmail.com>
2022-08-07 10:07:13 +0300

    add navbar to home

--- portfolio/src/Pages/Home.js
+++ portfolio/src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
-//import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 
 const Home = () => {
 
   return (
-    <div>Home</div>
+    <div>Home
+      <Navbar />
+    </div>
   )
 }
 
