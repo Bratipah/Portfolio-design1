8479fc84253b2c0fbf5d97f20178f08b0e56aa09
Bratipah <bratipahmh@gmail.com>
2022-08-07 09:47:18 +0300

    add pages and components

---
+++ portfolio/src/Pages/Work.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import Navbar from './components/Navbar';
+
+const Work = () => {
+  return (
+    <div>
+        work
+    </div>
+  )
+}
+
+export default Work
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NavbarStyle.css";
+import {Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <Link></Link>
+    </div>
+  )
+}
+
+export default Navbar
