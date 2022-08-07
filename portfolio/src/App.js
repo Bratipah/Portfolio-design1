7bbf53f29b5c38e1f574991f9668336b2ceed899
Bratipah <bratipahmh@gmail.com>
2022-08-07 09:48:13 +0300

    add links to pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./Pages/Home";
+import Work from "./Pages/Work";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element = {<Home />} />
+        <Route path ="/work" element = {<Work />} />
+        <Route path ="/blog" element = {<Blog />} />
+        <Route path ="/contact" element = {<Contact />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
